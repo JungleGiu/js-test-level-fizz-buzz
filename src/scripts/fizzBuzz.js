39548579fce210ff497a90825bef280c44d02ff1
Giu Eminente <giulemi210@gmail.com>
2025-07-29 11:06:28 +0200

    tests for fizzBuzz function written and passed

--- src/scripts/fizzBuzz.js
+++ src/scripts/fizzBuzz.js
@@ -1,45 +1,43 @@
-
-    export const fizzBuzz = (number) => {
-let response = {
-    status : '',
-    message :'',
-    data :{
-        number: 0,
-        result : ''
-    }
-}
-if (isNaN(number)|| number < 0) {
-    response.status = 'error';
-    response.message = 'Invalid input';
-    response.data.number = number;
-    response.data.result = 'Invalid input';
+export const fizzBuzz = (number) => {
+  let response = {
+    status: "",
+    message: "",
+    data: {
+      input: 0,
+      output: "",
+    },
+  };
+  if (isNaN(number) || number < 0) {
+    response.status = "error";
+    response.message = "Invalid input";
+    response.data.input = number;
+    response.data.output = "Invalid input";
     return response;
-}
-if (number % 3 === 0 && number % 5 === 0) {
-    response.status = 'ok';
-    response.message = 'The number is divisible by 3 and 5';
-    response.data.number = number;
-    response.data.result = 'FizzBuzz';
+  }
+  if (number % 3 === 0 && number % 5 === 0) {
+    response.status = "ok";
+    response.message = "The number is divisible by 3 and 5";
+    response.data.input = number;
+    response.data.output = "FizzBuzz";
     return response;
-}
-if (number % 3 === 0) {
-    response.status = 'ok';
-    response.message = 'The number is divisible by 3';
-    response.data.number = number;
-    response.data.result = 'Fizz';
+  }
+  if (number % 3 === 0) {
+    response.status = "ok";
+    response.message = "The number is divisible by 3";
+    response.data.input = number;
+    response.data.output = "Fizz";
     return response;
-}
-if (number % 5 === 0) {
-    response.status = 'ok';
-    response.message = 'The number is divisible by 5';
-    response.data.number = number;
-    response.data.result = 'Buzz';
+  }
+  if (number % 5 === 0) {
+    response.status = "ok";
+    response.message = "The number is divisible by 5";
+    response.data.input = number;
+    response.data.output = "Buzz";
     return response;
-}
-response.status = 'ok';
-response.message = 'The number is not divisible by 3 or 5';
-response.data.number = number;
-response.data.result = number;
-return response;
-
-    }
+  }
+  response.status = "ok";
+  response.message = "The number is not divisible by 3 or 5";
+  response.data.input = number;
+  response.data.output = number;
+  return response;
+};
