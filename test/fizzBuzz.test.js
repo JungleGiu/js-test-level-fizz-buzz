d2836119b12e321202be644aa3b557a5fe40d9d3
Giu Eminente <giulemi210@gmail.com>
2025-07-29 11:07:50 +0200

    tests for fizzBuzz function written and passed

--- test/fizzBuzz.test.js
+++ test/fizzBuzz.test.js
@@ -51,17 +51,18 @@ describe("FizzBuzz test for multiples of 3 and 5", () => {
   });
 
   test("returns number when is not multiple of 3 and 5", () => {
-             let valor_entrada = 8;
-            let respuesta_esperada = {
-                status: "ok", 
-                message: "The number is not divisible by 3 or 5",
-                data: {
-                    input: 8, 
-                    output: 8
-                }}
-            let resultado = fizzBuzz(valor_entrada);
-            expect(typeof resultado.data.input).toBe("number"); 
-            expect(resultado).toEqual(respuesta_esperada); 
-            expect(resultado.data.output).toBe(8); 
+    let valor_entrada = 8;
+    let respuesta_esperada = {
+      status: "ok",
+      message: "The number is not divisible by 3 or 5",
+      data: {
+        input: 8,
+        output: 8,
+      },
+    };
+    let resultado = fizzBuzz(valor_entrada);
+    expect(typeof resultado.data.input).toBe("number");
+    expect(resultado).toEqual(respuesta_esperada);
+    expect(resultado.data.output).toBe(8);
   });
 });
